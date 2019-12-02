5576cc510e0fc7d41509ce69bd7d46bdf006ee75
XUE Cheng <xuecheng@nuaa.edu.cn>
2019-12-02 11:16:33 +0800

    add boundary check to MAPGPOptimizer

--- src/gpmodel/MAPGPOptimizer.jl
+++ src/gpmodel/MAPGPOptimizer.jl
@@ -9,9 +9,9 @@ end
 
 function MAPGPOptimizer(;
     global_interval::Int=10,
-    global_optimizer::NLOptimizer=ISRESOptimizer(;maxeval=10000, ftol_rel=1e-2),
+    global_optimizer::NLOptimizer=ISRESOptimizer(;maxeval=10000),
     local_interval::Int=1,
-    local_optimzier::NLOptimizer=LBFGSOptimizer(;maxeval=100),
+    local_optimzier::NLOptimizer=LBFGSOptimizer(;maxeval=1000),
     domean = true, kern = true, noise = false, lik = false, 
     meanbounds = nothing, kernbounds = nothing,
     noisebounds = nothing, likbounds = nothing)
@@ -41,6 +41,15 @@ function optimizemodel!(o::MAPGPOptimizer, model::GP.GPBase, n::Int=1)
     o.count += n
 end
 
+function check_bounds(name, x, lb, ub)
+    d = ub-lb
+    if any(abs.((x .- ub) ./ d) .< 1e-3)
+        @warn "Found optimum `$name`(=$x) is close to the upper boundary (=$ub), You may try enlarging the `$name` bounds"
+    elseif any(abs.((x .- lb) ./ d) .< 1e-3)
+        @warn "Found optimum `$name`(=$x) is close to the lower boundary (=$lb), You may try enlarging the `$name` bounds"
+    end
+end
+
 function optimizemodel!(gp::GP.GPBase, opt::NLOptimizer; 
     domean, kern, noise, lik, meanbounds, kernbounds, noisebounds, likbounds)
     params_kwargs = GP.get_params_kwargs(gp; domean=domean, kern=kern, noise=noise, lik=lik)
@@ -64,5 +73,9 @@ function optimizemodel!(gp::GP.GPBase, opt::NLOptimizer;
     fx, x, ret = maximize(opt, f, lb, ub, GP.get_params(gp; params_kwargs...))
     GP.set_params!(gp, x; params_kwargs...)
     GP.update_target_and_dtarget!(gp; params_kwargs...)
+    domean && check_bounds("mean", GP.get_params(gp.mean), meanbounds...)
+    kern && check_bounds("kernel", GP.get_params(gp.kernel), kernbounds...)
+    noise && check_bounds("logNoise", GP.get_params(gp.logNoise), noisebounds...)
+    lik && checkbounds("lik", GP.get_params(gp.lik), likbounds...)
     return fx, x, ret
 end
