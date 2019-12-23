59f2506c0d43a0715363b04bbab4dee91526aec7
XUE Cheng <xuecheng@nuaa.edu.cn>
2019-12-23 19:54:11 +0800

    add `retry` to `acquire`

--- src/EGO.jl
+++ src/EGO.jl
@@ -1,6 +1,7 @@
 import Base: append!, ndims, length, isempty, getindex
-import Sobol
-
+struct NoFeasibleInfill <: Exception
+    msg::String
+end
 
 struct EGO{S,KRG <: Kriging,AQ <: SingleObjAquisition,AC <: Acquire,LB,UB}
     krg::KRG
@@ -45,8 +46,22 @@ optimum(ego::EGO{Min}) = ego.krg[argmin(ego.krg.gps[1].y)]
 
 optimum(ego::EGO{Max}) = ego.krg[argmax(ego.krg.gps[1].y)]
 
-function acquire(ego::EGO, x_cache::AbstractVector...)
-    acquire(ego.krg, ego.aquisition, ego.acquire, ego.lowerbounds, ego.upperbounds, x_cache...)
+function acquire(ego::EGO, x_cache::AbstractVector...; retry::Integer = 5, verbose::Bool=false)
+    x, fx = acquire(ego.krg, ego.aquisition, ego.acquire, ego.lowerbounds, ego.upperbounds, x_cache...)
+    if fx==0 
+        if retry > 1
+            if verbose
+                @info "Cannot find feasible infill. retry($(retry-1))"
+            end
+            return acquire(ego, x_cache...; retry=retry-1, verbose=verbose)
+        else
+            throw(NoFeasibleInfill("Cannot find feasible infill. Kriging model is converged."))
+        end
+    end
+    if verbose
+        @info "Infill acquired, x=$x, fx=$fx"
+    end
+    x, fx
 end
 
 history(ego::EGO) = get_samples(ego.krg)
